607a00b40c41e4674dd49e740d7ddeb841e2655d
Paweł Janeczek <pawel.janeczek@polidea.com>
2015-04-10 11:22:49 +0200

    removed unused imports

--- hierarchyviewer/src/main/java/com/polidea/hierarchyviewer/internal/provider/HierarchyViewProvider.java
+++ hierarchyviewer/src/main/java/com/polidea/hierarchyviewer/internal/provider/HierarchyViewProvider.java
@@ -1,10 +1,7 @@
 package com.polidea.hierarchyviewer.internal.provider;
 
 
-import android.os.Build;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
